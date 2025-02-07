c6ca4fb422f293a242cc95e10ca03aece275abb3
pathway <prabin@pathway.com.np>
2025-02-07 12:25:57 +0545

    class updated

--- AndroidIosLearner/iosApp/iosApp/LanguageFamiliar/ClassExample.swift
+++ AndroidIosLearner/iosApp/iosApp/LanguageFamiliar/ClassExample.swift
@@ -10,8 +10,22 @@ import SwiftUI
 
 class ClassExample {
     //var name: String = ""
-    var name = ""
-    let fixedName = "SwiftUI"
+    var name = ""              // var means mutable variable
+    let fixedName = "SwiftUI" // let means constant - non mutable
+    let size:Int = 100
+    let isAwesome:Bool = true
+    let pi:Float = 3.14
+    let poi:Double = 3.123243242
+    let letters: [String] = [
+        "A", "B", "C", "D"
+        ]
+    let numbers: [Int] = [1, 2, 3, 4]
+    let point: [CGPoint] = [
+    CGPoint(x: 100, y: 200),
+    CGPoint(x: 300, y: 400),
+    CGPoint(x: 500, y: 600),
+    ]
+        
     
     func printName() {
         debugPrint("\(name) \(fixedName) is awesome")
@@ -31,5 +45,9 @@ class ClassExample {
     
     func getMeSomething() -> String {
         "Hello Galaxie"
+    }
+    
+    func toString(){
+        debugPrint("\(numbers) \(point)")
     }
 }
