46130c055719ddae76c8a70f0706d9fcfe88147a
pathway <prabin@pathway.com.np>
2025-02-10 11:09:51 +0545

    description added for variables

--- AndroidIosLearner/iosApp/iosApp/LanguageFamiliar/ClassExample.swift
+++ AndroidIosLearner/iosApp/iosApp/LanguageFamiliar/ClassExample.swift
@@ -9,18 +9,23 @@
 import SwiftUI
 
 class ClassExample {
-    //var name: String = ""
-    var name = ""              // var means mutable variable
-    let fixedName = "SwiftUI" // let means constant - non mutable
-    let size:Int = 100
-    let isAwesome:Bool = true
-    let pi:Float = 3.14
-    let poi:Double = 3.123243242
+    //var name: String = "" // you can define the datatype like in kotlin or ignore it
+    var name = ""              // var means mutable variable - var in kotlin as well
+    let fixedName = "SwiftUI" // let means constant - non mutable - val in kotlin
+    let size:Int = 100             //for small integer size - Int in kotlin
+    let bigsize:Int64 = 100000000000 // for bigger integer sizes Int32 Int16 choose according to your requirement - Long in kotlin
+    let isAwesome:Bool = true      // boolean in kotlin
+    let pi:Float = 3.14                 // float in kotlin as well
+    let poi:Double = 3.123243242        // Double in kotlin as well
+    // for array list its the same way
+    // val arrayListLetters = ArrayList<String>() in kotlin // there are other way as well to make dynamic list in kotlin
+    let arrayListLetters = [String]()
+    //  for simple array of string,
     let letters: [String] = [
         "A", "B", "C", "D"
         ]
-    let numbers: [Int] = [1, 2, 3, 4]
-    let point: [CGPoint] = [
+    let numbers: [Int] = [1, 2, 3, 4]   // for array of int
+    let point: [CGPoint] = [             // for array of object
     CGPoint(x: 100, y: 200),
     CGPoint(x: 300, y: 400),
     CGPoint(x: 500, y: 600),
